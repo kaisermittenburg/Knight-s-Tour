d0f4c4a7a92733bd815808882ab5747ff4c7959b
Kaiser Mittenburg <Kaiser Mittenburg>
2017-06-06 20:32:26 -0500

    Add project files.

---
+++ Knight's Tour/Knight_s_Tour/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Knight_s_Tour
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new Knight_s_Tour.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
